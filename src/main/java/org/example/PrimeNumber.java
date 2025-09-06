7d8e2f09cbd9d6a20f9fca6ad6141cdbe7b5d4a1
Khanh Tran <khanh.tx@live.com>
2025-09-06 11:02:01 +0700

    Add code

---
+++ src/main/java/org/example/PrimeNumber.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class PrimeNumber {
+    public boolean check(int number) {
+        if(number>=0 && number<=2){
+            return false;
+        }
+
+        if(number<0)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        boolean result = true;
+        for(int i=2;i<=Math.sqrt(number);i++){
+            if(number%i==0){
+                result = false;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/org/example/PrimeNumberTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeNumberTest {
+    @Test
+    public void check_number_less_then_2_is_not_prime_number(){
+        //Arrange
+        PrimeNumber primeNumber = new PrimeNumber();
+        int number = 2;
+        boolean expected = false;
+
+        //Action
+        boolean actual = primeNumber.check(number);
+
+        //Assertion
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void check_return_true_if_number_is_prime_number(){
+        //Arrange
+        PrimeNumber primeNumber = new PrimeNumber();
+        int number = 11;
+        boolean expected = true;
+        //Action
+        boolean actual = primeNumber.check(number);
+
+        //Assertion
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void check_return_false_if_number_is_not_prime_number(){
+        //Arrange
+        PrimeNumber primeNumber = new PrimeNumber();
+        int number = 4;
+        boolean expected = false;
+        //Action
+        boolean actual = primeNumber.check(number);
+
+        //Assertion
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void check_throw_exception_if_number_is_negative(){
+        PrimeNumber primeNumber = new PrimeNumber();
+        int number = -1;
+
+        assertThrows(IllegalArgumentException.class, () -> primeNumber.check(number));
+
+    }
+}
